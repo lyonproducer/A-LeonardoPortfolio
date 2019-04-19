031fdfbd4e73635b4bbef19b84c652d3827ba917
Leonardo <hernandezleonardo085@gmail.com>
2019-04-19 03:29:19 -0400

    Leo  acerca  de mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Route, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { CoverProfileComponent } from './components/cover-profile/cover-profile.component';
+
+
+const routes: Route[] = [
+
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+
+  {path: 'index', component: AboutMeComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    CoverProfileComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
